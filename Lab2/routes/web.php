40d562d1e03ece2bf4d6f601a0cbab7669110389
hussein-elmlah <husseinelmlah11@gmail.com>
2024-04-30 14:52:06 +0300

    working version

--- Lab2/routes/web.php
+++ Lab2/routes/web.php
@@ -23,5 +23,5 @@ Route::get("/posts/create",[PostController::class,'create'])->name('post.create'
 Route::post("/posts/store",[PostController::class,'store'])->name('post.store');
 Route::get("/posts/{id}", [PostController::class,'show'])->name('post.show');
 Route::get("/posts/edit/{id}", [PostController::class,'edit'])->name('post.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('post.update');
+Route::put("/posts/update/{id}", [PostController::class,'update'])->name('post.update');
 Route::delete("/posts/delete/{id}", [PostController::class,'destroy'])->name('post.delete');
